aac5b420563d371138bb6855c5e5cf5bb99abc65
jeremyfelt <jeremy.felt@gmail.com>
2014-11-13 14:04:03 -0800

    Setup the plugin header

--- wsuwp-mailchimp.php
+++ wsuwp-mailchimp.php
@@ -1,6 +1,14 @@
 <?php
+/*
+Plugin Name: WSU MailChimp
+Plugin URI: http://web.wsu.edu/wordpress/plugins/wsu-mailchimp/
+Description: Allow site visitors to subscribe to a mailing list.
+Author: washingtonstateuniversity, jeremyfelt
+Version: 0.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
 
-// plugin header
 // shortcode
 // widget
 
